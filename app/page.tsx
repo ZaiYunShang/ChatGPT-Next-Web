51e2c67d26a459aeb7587494f098f9d26ea288d7
unknown <1626350305@qq.com>
2023-05-25 16:21:31 +0800

    11111

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
-
+import * as jwt from 'jsonwebtoken';
 import { Home } from "./components/home";
 
 import { getServerSideConfig } from "./config/server";
@@ -7,11 +7,14 @@ import { getServerSideConfig } from "./config/server";
 const serverConfig = getServerSideConfig();
 
 export default async function App() {
+  const payload = { username: 'exampleuser', role: 'admin' };
+  const secret = 'mysecretkey';
+  const token = jwt.sign(payload, secret, { expiresIn: '1h' });
   // return (
   //   <>
   //     <Home />
   //     {serverConfig?.isVercel && <Analytics />}
   //   </>
   // );
-  return ("My App");
+  return (token);
 }
